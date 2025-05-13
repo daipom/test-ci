8955811e03327716b8b6ee9ae6867e20eb750cc6
Daijiro Fukuda <fukuda@clear-code.com>
2025-05-13 19:26:53 +0900

    stop-commands
    https://docs.github.com/ja/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions?tool=powershell

--- test.ps1
+++ test.ps1
@@ -1,6 +1,9 @@
 $ErrorActionPreference = "Stop"
 Set-PSDebug -Trace 1
 
+$stopMarker = New-Guid
+Write-Output "::stop-commands::$stopMarker"
+
 "error" | Out-Null
 "[error]" | Out-Null
 "error" | Out-Null
